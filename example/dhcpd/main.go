d69e57e261423916c35fc65716f3717a9bb61c2a
wkh <746906141@qq.com>
2024-07-11 10:06:00 +0800

    fix

--- example/dhcpd/main.go
+++ example/dhcpd/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/yudaiyan/go-dhcpd/dhcpd"
 )
 
 func main() {
-	dhcpd.CreateServer("tap-dPeTE")
+	var ifname string
+	flag.StringVar(&ifname, "ifname", "tap-dPeTE", "接口名")
+	if err := dhcpd.CreateServer(ifname); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
